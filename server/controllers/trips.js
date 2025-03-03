1f776694b9c09fa787aedf4a6a17cd8e31cb7ee5
nestor-remo <nestiray12345@gmail.com>
2025-03-02 22:06:49 -0800

    initial commit

---
+++ server/controllers/trips.js
@@ -0,0 +1,23 @@
+import { pool } from '../config/database.js'
+
+const createTrip = async (req, res) => {
+    try {
+        
+    }
+}
+
+const getTrips = async (req, res) => {
+
+}
+
+const getTrip = async (req, res) => {
+
+}
+
+const updateTrip = async (req, res) => {
+
+}
+
+const deleteTrip = async (req, res) => {
+
+}
