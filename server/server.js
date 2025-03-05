54cb1b4d1eb9d9e49bd905f49426aa3bc72d5240
nestor-remo <nestiray12345@gmail.com>
2025-03-04 21:26:08 -0800

    add controller and set up trips routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import tripsRoutes from './routes/trips.js';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">Welcome to Flight-Aid</h1>');
 });
+
+app.use('/trips', tripsRoutes);
 
 const PORT = process.env.PORT || 3001;
 
