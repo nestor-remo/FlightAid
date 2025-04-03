fa02e9eac93851dea3e43cfc7ef2d598971ad372
nestor-remo <nestiray12345@gmail.com>
2025-04-02 19:23:12 -0700

    reframe models

--- server/config/models.js
+++ server/config/models.js
@@ -3,7 +3,6 @@ import './dotenv.js'
 import { fileURLToPath } from 'url'
 import path, { dirname } from 'path'
 import fs from 'fs'
-import { create } from 'domain'
 
 const currentPath = fileURLToPath(import.meta.url)
 const tripsFile = fs.readFileSync(path.join(dirname(currentPath), '../config/data/data.json'))
@@ -30,37 +29,8 @@ const createTripsTable = async () => {
         console.error('Trips table not created', err)
     }
 }
-  
-const seedTripsTable = async () => {
-    await createTripsTable()
 
-    tripsData.forEach ((trip) => {
-        const insertQuery = {
-            text: `INSERT INTO trips (title, description, img_url, num_days, start_date, end_date, total_cost) VALUES ($1, $2, $3, $4, $5, $6, $7)`
-        }
-
-        const values = [
-            trip.title,
-            trip.description,
-            trip.img_url,
-            trip.num_days,
-            trip.start_date,
-            trip.end_date,
-            trip.total_cost
-        ]
-
-        pool.query(insertQuery, values, (err, res) => {
-            if (err) {
-                console.error('error inserting trip', err)
-                return
-            }
-            console.log(`$[trip.title} inserted successfully`)
-        })
-    })
-
-}
-
-// seedTripsTable()
+createTripsTable()
 
 const createDestinationsTable = async () => {
     const createDestinationsTableQuery = `
@@ -105,62 +75,21 @@ const createActivitiesTable = async () => {
 
 createActivitiesTable()
 
-const createTripsDestinationsTable = async () => {
-    const createTripsDestinationsTableQuery = `
-        CREATE TABLE IF NOT EXISTS trips_destinations (
-            trip_id int NOT NULL,
-            destination_id int NOT NULL,
-            PRIMARY KEY (trip_id, destination_id),
-            FOREIGN KEY (trip_id) REFERENCES trips(id) ON UPDATE CASCADE,
-            FOREIGN KEY (destination_id) REFERENCES destinations(id) ON UPDATE CASCADE
-        );
-    `
-    try {
-        const res = await pool.query(createTripsDestinationsTableQuery)
-        console.log('Trips_destinations table created successfully')
-    } catch (err) {
-        console.error('Error creating trips_destinations table', err)      
-    }
-}
-
-createTripsDestinationsTable()
 
 const createUsersTable = async () => {
     const createUsersTableQuery = `
         CREATE TABLE IF NOT EXISTS users (
             id serial PRIMARY KEY,
-            githubid integer NOT NULL,
             username varchar(100) NOT NULL,
-            avatarurl varchar(500) NOT NULL,
-            accesstoken varchar(500) NOT NULL
+            email varchar(100) NOT NULL
         );
     `
     try {
         const res = await pool.query(createUsersTableQuery)
         console.log('Users table created successfully')
-    } catch (error) {
+    } catch (err) {
         console.error('Error creating users table', err)
     }    
 }
 
 createUsersTable()
-
-const createTripsUsersTable = async () => {
-    const createTripsUsersTableQuery = `
-        CREATE TABLE IF NOT EXISTS trips_users (
-            trip_id int NOT NULL,
-            user_id int NOT NULL,
-            PRIMARY KEY (trip_id, user_id),
-            FOREIGN KEY (trip_id) REFERENCES trips(id) ON UPDATE CASCADE,
-            FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE
-        );
-    `
-    try {
-        const res = await pool.query(createTripsUsersTableQuery)
-        console.log('Trips_users table created successfully')
-    } catch (err) {
-        console.error('Error creating trips_users table', err)
-    }
-}
-
-createTripsUsersTable()
